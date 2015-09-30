6a8e69b5ed506f8691ff95b3835da6e89150a3c4
peter <peter.k.nets@gmail.com>
2015-09-30 13:06:51 +0300

    Add 'Contact' page and js functionality

--- js/app.js
+++ js/app.js
@@ -7,15 +7,23 @@ function PageNavigation() {
     <!-- functions -->
     this.openHomePage = function() {
         $(".home-page-jumbo").show();
+        $(".contact-page-jumbo").hide();
         $(".shop-page-jumbo").hide();
     };
     this.openShopPage = function() {
         $(".home-page-jumbo").hide();
+        $(".contact-page-jumbo").hide();
         $(".shop-page-jumbo").show();
+    };
+    this.openContactPage = function() {
+        $(".home-page-jumbo").hide();
+        $(".shop-page-jumbo").hide();
+        $(".contact-page-jumbo").show();
     };
     <!-- events -->
     nav.on("click.nav", ".home-button", this.openHomePage);
     nav.on("click.nav", ".shop-button", this.openShopPage);
+    nav.on("click.nav", ".contact-button", this.openContactPage);
 }
 
 $(document).ready(function(){
