40333cba22b690d6ce8a2dcbb065b3d9eb7c900f
peter <peter.k.nets@gmail.com>
2015-09-30 12:48:54 +0300

    Add functions to switch between Home and Shop jumbo text

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 /**
  * Created by Peter on 30.09.2015.
  */
+function PageNavigation() {
+    <!-- vars -->
+    var nav = $(".nav");
+    <!-- functions -->
+    this.openHomePage = function() {
+        $(".home-page-jumbo").show();
+        $(".shop-page-jumbo").hide();
+    };
+    this.openShopPage = function() {
+        $(".home-page-jumbo").hide();
+        $(".shop-page-jumbo").show();
+    };
+    <!-- events -->
+    nav.on("click.nav", ".home-button", this.openHomePage);
+    nav.on("click.nav", ".shop-button", this.openShopPage);
+}
+
+$(document).ready(function(){
+    var pageNavigation = new PageNavigation();
+});
